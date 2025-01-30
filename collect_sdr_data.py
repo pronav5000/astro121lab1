2f169f8f170436d103e144cb34197277ab7d606e
Pranav <pranavkarthik@berkeley.edu>
2025-01-29 16:28:17 -0800

    script to collect sdr data, data files, notebook

---
+++ collect_sdr_data.py
@@ -0,0 +1,10 @@
+import ugradio
+import numpy as np
+
+SAMPLE_RATES = np.arange(1e6, 3.3e6, 1e5)
+for SAMPLE_RATE in SAMPLE_RATES:
+    sdr = ugradio.sdr.SDR(sample_rate = SAMPLE_RATE, fir_coeffs=np.array([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 2047]))
+    _ = sdr.capture_data(nblocks = 1)
+    data = sdr.capture_data(nblocks = 10)
+    sdr.close()
+    np.savez(f"{SAMPLE_RATE}_sdr_data.npz", data)
