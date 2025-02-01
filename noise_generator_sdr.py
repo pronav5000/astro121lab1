1fec1265525e81b80dec8817cedb3121a1b614dd
Pranav <pranavkarthik@berkeley.edu>
2025-02-01 13:52:22 -0800

    5.7

--- noise_generator_sdr.py
+++ noise_generator_sdr.py
@@ -1,6 +1,6 @@
 import ugradio
 import numpy as np
-SAMPLE_RATE = 1e6
+SAMPLE_RATE = 1.2e6
 sdr = ugradio.sdr.SDR(sample_rate = SAMPLE_RATE,fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
 _ = sdr.capture_data(nblocks=1)
 data = sdr.capture_data(nblocks=10)
