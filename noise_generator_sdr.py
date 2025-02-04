8c4eb677a0ff7ae293bbf4e29219236380024e0f
Pranav <pranavkarthik@berkeley.edu>
2025-02-03 16:21:48 -0800

    5.7 data

--- noise_generator_sdr.py
+++ noise_generator_sdr.py
@@ -4,6 +4,6 @@ SAMPLE_RATES = np.arange(1e6, 3.3e6, 1e5)
 for SAMPLE_RATE in SAMPLE_RATES:
     sdr = ugradio.sdr.SDR(sample_rate = SAMPLE_RATE,fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
     _ = sdr.capture_data(nblocks=1)
-    data = sdr.capture_data(nblocks=16)
+    data = sdr.capture_data(nblocks=100)
     sdr.close()
-    np.savez(f"noise_generator_data/{SAMPLE_RATE}_noise_generator_2filter_data.npz", data)
+    np.savez(f"noise_generator_data/{SAMPLE_RATE}_noise_generator_data.npz", data)
