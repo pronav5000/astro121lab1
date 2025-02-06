4cdbeacb42ef179f790c319183b20275aa244947
Pranav <pranavkarthik@berkeley.edu>
2025-02-05 18:35:25 -0800

    7.3.3

--- mixer_1.py
+++ mixer_1.py
@@ -5,8 +5,8 @@ required_rates = [2*1170000,2*1230000]
 additional_rates = np.arange(2.5e6, 3.3e6, 1e5)
 SAMPLE_RATES = np.sort(np.array(required_rates+list(additional_rates)))
 for SAMPLE_RATE in SAMPLE_RATES:
-    sdr = ugradio.sdr.SDR(sample_rate = SAMPLE_RATE,fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+    sdr = ugradio.sdr.SDR(sample_rate = SAMPLE_RATE,direct=False)
     _ = sdr.capture_data(nblocks=1)
     data = sdr.capture_data(nblocks=100)
     sdr.close()
-    np.savez(f"mixer_data/{SAMPLE_RATE}_7.3.2_imag_diff_freq_data.npz", data)
+    np.savez(f"mixer_data/{SAMPLE_RATE}_7.3.3_imag_sum_freq_data.npz", data)
