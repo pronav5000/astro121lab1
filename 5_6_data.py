222b80d65e030d0aadfd6e4f50381dc21d8856d0
Pranav <pranavkarthik@berkeley.edu>
2025-02-07 17:27:50 -0800

    test

---
+++ 5_6_data.py
@@ -0,0 +1,9 @@
+import ugradio
+import numpy as np
+
+SAMPLE_RATE = 1e6
+sdr=ugradio.sdr.SDR(sample_rate=SAMPLE_RATE, fir_coeffs=np.array([0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,2047]))
+_=sdr.capture_data(nblocks=1)
+data=sdr.capture_data(nblocks=100)
+sdr.close()
+np.savez("sdr_data/sdr_nw2_data.npz", data)
