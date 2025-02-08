3c97d2982a83a6fa9d5ed4ab4379fa7851948109
Pranav <pranavkarthik@berkeley.edu>
2025-02-07 17:37:56 -0800

    test

--- 5_6_data.py
+++ 5_6_data.py
@@ -6,4 +6,4 @@ sdr=ugradio.sdr.SDR(sample_rate=SAMPLE_RATE, fir_coeffs=np.array([0,0,0,0,0,0,0,
 _=sdr.capture_data(nblocks=1)
 data=sdr.capture_data(nblocks=100)
 sdr.close()
-np.savez("sdr_data/sdr_nw1_data.npz", data)
+np.savez("sdr_data/sdr_nw4_data.npz", data)
